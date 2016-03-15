236daa164eceb6f8ea9d80cd4e3eb24d9828a527
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-03-15 19:17:47 +0200

    added malware blocking/social media buttons and anti-tracking filters

--- options.js
+++ options.js
@@ -91,6 +91,104 @@ function insertCSS(id, file){
 }
 
 
+/**
+* from chromeadblockplus/firstRun.js
+**/
+
+"use strict";
+(function()
+{
+    // Load subscriptions for features
+    var featureSubscriptions = [
+        {
+            feature: "malware",
+            homepage: "http://malwaredomains.com/",
+            title: "Malware Domains",
+            url: "https://easylist-downloads.adblockplus.org/malwaredomains_full.txt"
+        },
+        {
+            feature: "social",
+            homepage: "https://www.fanboy.co.nz/",
+            title: "Fanboy's Social Blocking List",
+            url: "https://easylist-downloads.adblockplus.org/fanboy-social.txt"
+        },
+        {
+            feature: "tracking",
+            homepage: "https://easylist.adblockplus.org/",
+            title: "EasyPrivacy",
+            url: "https://easylist-downloads.adblockplus.org/easyprivacy.txt"
+        }
+    ];
+
+    function onDOMLoaded()
+    {
+         // Set up feature buttons linked to subscriptions
+        featureSubscriptions.forEach(initToggleSubscriptionButton);
+        updateToggleButtons();
+        ext.onMessage.addListener(function(message)
+        {
+            if (message.type == "subscriptions.listen")
+            {
+                updateToggleButtons();
+            }
+        });
+        ext.backgroundPage.sendMessage({
+            type: "subscriptions.listen",
+            filter: ["added", "removed", "updated", "disabled"]
+        });
+    }
+
+    function initToggleSubscriptionButton(featureSubscription)
+    {
+        var feature = featureSubscription.feature;
+
+        var element = E("toggle-" + feature);
+        element.addEventListener("click", function(event)
+        {
+         //sendingMessage is blocking the css transition
+         setTimeout(function(){
+             ext.backgroundPage.sendMessage({
+                 type: "subscriptions.toggle",
+                 url: featureSubscription.url,
+                 title: featureSubscription.title,
+                 homepage: featureSubscription.homepage
+             });
+         },300);
+
+        }, false);
+    }
+
+    function updateToggleButtons()
+    {
+        ext.backgroundPage.sendMessage({
+            type: "subscriptions.get",
+            downloadable: true,
+            ignoreDisabled: true
+        }, function(subscriptions)
+        {
+            var known = Object.create(null);
+            for (var i = 0; i < subscriptions.length; i++)
+                known[subscriptions[i].url] = true;
+            for (var i = 0; i < featureSubscriptions.length; i++)
+            {
+                var featureSubscription = featureSubscriptions[i];
+                updateToggleButton(featureSubscription.feature, featureSubscription.url in known);
+            }
+        });
+    }
+    function updateToggleButton(feature, isEnabled)
+    {
+        console.log(feature, isEnabled);
+        var button = E("toggle-" + feature);
+        if (isEnabled)
+            button.checked = true;
+        else
+            button.checked = false;
+    }
+    document.addEventListener("DOMContentLoaded", onDOMLoaded, false);
+})();
+
+
 
 
 
