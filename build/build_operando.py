22ebcdf2de99790680dfcaef560e5f15f9becc3e
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-04-29 15:14:09 +0300

    small build update

--- build/build_operando.py
+++ build/build_operando.py
@@ -21,10 +21,15 @@ def buildOperando():
 	return
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BASE_DIR = os.path.join(BASE_DIR, "..","..");
+BASE_DIR = os.path.join(BASE_DIR, "..");
 os.chdir(BASE_DIR)
-#print os.getcwd()
+extensionFolder = os.getcwd()
+BASE_DIR = os.path.join(BASE_DIR, "..");
+os.chdir(BASE_DIR)
+
+#print extensionFolder
 #sys.exit(0)
+
 
 if not os.path.exists(os.path.join(BASE_DIR,"adblockpluschrome")):
     	os.system('git clone "https://github.com/adblockplus/adblockpluschrome"')
@@ -33,54 +38,56 @@ if not os.path.exists(os.path.join(BASE_DIR,"adblockpluschrome")):
 
 files = [
 {
-	"src":"op-chrome-ext",
+	"src":extensionFolder,
 	"dest":"adblockpluschrome/operando",
 	"is_directory":1
 },
 {
-  "src": "op-chrome-ext/build/override/metadata.common",
+  "src": "build/override/metadata.common",
   "dest": "adblockpluschrome/metadata.common",
   "is_directory":0
 }, {
-  "src": "op-chrome-ext/build/override/metadata.chrome",
+  "src": "build/override/metadata.chrome",
   "dest": "adblockpluschrome/metadata.chrome",
   "is_directory":0
 }, {
-  "src": "op-chrome-ext/build/override/packagerChrome.py",
+  "src": "build/override/packagerChrome.py",
   "dest": "adblockpluschrome/buildtools/packagerChrome.py",
   "is_directory":0
 }, {
-  "src": "op-chrome-ext/build/override/popup.html",
+  "src": "build/override/popup.html",
   "dest": "adblockpluschrome/popup.html",
   "is_directory":0
 }, {
-  "src": "op-chrome-ext/build/override/popup.css",
+  "src": "build/override/popup.css",
   "dest": "adblockpluschrome/skin/popup.css",
   "is_directory":0
 },
  {
-  "src": "op-chrome-ext/build/override/firstRun.html",
+  "src": "build/override/firstRun.html",
   "dest": "adblockpluschrome/adblockplusui/firstRun.html",
   "is_directory":0
 },
 {
-  "src": "op-chrome-ext/build/override/adblockplus/chrome/locale/en-US/meta.properties",
+  "src": "build/override/adblockplus/chrome/locale/en-US/meta.properties",
   "dest": "adblockpluschrome/adblockplus/chrome/locale/en-US/meta.properties",
   "is_directory":0
 }]
+
 
 
 for file in files:
 	if file['is_directory'] == 1:
 	    if os.path.exists(os.path.join(BASE_DIR,file['dest'])):
                 shutil.rmtree(os.path.join(BASE_DIR,file['dest']))
-	    copytree(os.path.join(BASE_DIR,file['src']), os.path.join(BASE_DIR,file['dest']),ignore=ignore_patterns('.gitignore', '.git','.idea'))
+	    copytree(file['src'], os.path.join(BASE_DIR,file['dest']),ignore=ignore_patterns('.gitignore', '.git','.idea'))
 	else:
-		shutil.copy2(os.path.join(BASE_DIR,file['src']), os.path.join(BASE_DIR,file['dest']))
+		shutil.copy2(os.path.join(extensionFolder,file['src']), os.path.join(BASE_DIR,file['dest']))
 
 buildOperando()
 if os.path.exists(os.path.join(BASE_DIR,"devenv.chrome.extension")):
     shutil.rmtree(os.path.join(BASE_DIR,"devenv.chrome.extension"))
 copytree(os.path.join(BASE_DIR,"adblockpluschrome/devenv.chrome"), os.path.join(BASE_DIR,"devenv.chrome.extension"))
 
+print "Buld finished! Drag and drop devenv.chrome into your chrome extensions page"
 
