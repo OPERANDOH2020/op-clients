6dec6ed42dbe3f9611d774f9159b505e66dc4d9e
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-05-04 19:51:55 +0300

    cleanup android app

---
+++ op-clients-android/app/src/main/java/eu/operando/fragment/DrawerFragment.java
@@ -0,0 +1,48 @@
+package eu.operando.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.greenrobot.eventbus.Subscribe;
+
+import eu.operando.R;
+import eu.operando.events.EventLoginPage;
+import eu.operando.events.EventSignIn;
+import eu.operando.util.SharedPreferencesService;
+
+/**
+ * Created by raluca on 08.04.2016.
+ */
+public class DrawerFragment extends Fragment {
+
+    TextView emailTV ;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle saveInstanceState)
+
+    {
+        View v = inflator.inflate(R.layout.fragment_navigation_drawer, container, false);
+        initUI (v);
+        return v;
+    }
+
+    private void initUI (View v){
+        emailTV = (TextView) v.findViewById(R.id.emailTV);
+    }
+
+    @Subscribe
+    public void onEvent (EventSignIn event ) {
+        emailTV.setText(SharedPreferencesService.getInstance(getActivity()).getUserEmail());
+    }
+
+}
