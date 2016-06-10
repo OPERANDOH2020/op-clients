e51565a6f9321578ce131cbff263cb90e9403a35
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-06-10 20:13:59 +0300

    added radio buttons to questionnaire

--- op-clients-chrome-ext/modules/wizard-questionnaire.js
+++ op-clients-chrome-ext/modules/wizard-questionnaire.js
@@ -11,78 +11,81 @@
  */
 
 
-angular.module('operando').controller('FormController', function($scope) {
+angular.module('operando').controller('FormController', function ($scope) {
 
     $scope.schema = {
-            type: "object",
-            properties: {
-                audience:{
-                    title:"Do you prefer your social network posts and timelines to reach the widest possible audience?",
-                    type:"string",
-                    enum:["Yes","No, prefer to restrict to friends"]
-                },
-                personalized_ads:{
-                    title:"Do you like personalized ads?",
-                    type:"string",
-                    enum:["Yes","No"]
-                },
-                phone_email_share:{
-                    title:"Is it OK with you that your phone number and email address can be used to find and contact you on the social networks?",
-                    type:"string",
-                    enum:["Yes","No"]
-                },
-                privacy:{
-                    title:"What is more important  – privacy or ease of use?",
-                    type:"string",
-                    enum:["Privacy","Easy of use"]
-                },
-
-                freebies:{
-                    title:"What is more important – privacy or freebies that you can get on the Web?",
-                    type:"string",
-                    enum:["Privacy","Freebies"]
-                },
-                experience:{
-                    title:"What is more important – privacy or personalization of your web experience?",
-                    type:"string",
-                    enum:["Privacy","Web Experience"]
-                },
-                share:{
-                    title:"Do you like sharing everything with friends?",
-                    type:"string",
-                    enum:["Yes","No"]
-                },
-                friends_share:{
-                    title:"Do you enjoy it when friends share your posts with their friends?",
-                    type:"string",
-                    enum:["Yes","No"]
-                },
-                game_experience:{
-                    title:"Do you like to share your online game experience with friends?",
-                    type:"string",
-                    enum:["Yes","No"]
-                },
-                cv_share:{
-                    title:"Do you like your CV to be exposed to as many people as possible, or do you prefer to limit it as much as possible?",
-                    type:"string",
-                    enum:["Expose it","Limit it"]
-                }
-
-
-            }
-        };
-
-        $scope.form = [
-            "*",
-            {
-                items:["cv_share","game_experience"],
-                type: "radios"
+        type: "object",
+        properties: {
+            audience: {
+                title: "Do you prefer your social network posts and timelines to reach the widest possible audience?",
+                type: "string",
+                enum: ["Yes", "No, prefer to restrict to friends"]
             },
-            {
-                type: "submit",
-                title: "Save"
-            }
-        ];
+            personalized_ads: {
+                title: "Do you like personalized ads?",
+                type: "string",
+                enum: ["Yes", "No"]
+            },
+            phone_email_share: {
+                title: "Is it OK with you that your phone number and email address can be used to find and contact you on the social networks?",
+                type: "string",
+                enum: ["Yes", "No"]
+            },
+            privacy: {
+                title: "What is more important  – privacy or ease of use?",
+                type: "string",
+                enum: ["Privacy", "Easy of use"]
+            },
 
-        $scope.model = {};
+            freebies: {
+                title: "What is more important – privacy or freebies that you can get on the Web?",
+                type: "string",
+                enum: ["Privacy", "Freebies"]
+            },
+            experience: {
+                title: "What is more important – privacy or personalization of your web experience?",
+                type: "string",
+                enum: ["Privacy", "Web Experience"]
+            },
+            share: {
+                title: "Do you like sharing everything with friends?",
+                type: "string",
+                enum: ["Yes", "No"]
+            },
+            friends_share: {
+                title: "Do you enjoy it when friends share your posts with their friends?",
+                type: "string",
+                enum: ["Yes", "No"]
+            },
+            game_experience: {
+                title: "Do you like to share your online game experience with friends?",
+                type: "string",
+                enum: ["Yes", "No"]
+            },
+            cv_share: {
+                title: "Do you like your CV to be exposed to as many people as possible, or do you prefer to limit it as much as possible?",
+                type: "string",
+                enum: ["Expose it", "Limit it"]
+            }
+
+
+        }
+    };
+
+    $scope.form = [];
+
+    for (var key in $scope.schema.properties) {
+        $scope.form.push({
+            key: key,
+            type: "radios"
+        })
+    }
+
+    $scope.form.push({
+        type: "submit",
+        title: "Save"
     });
+
+
+    $scope.model = {};
+});
