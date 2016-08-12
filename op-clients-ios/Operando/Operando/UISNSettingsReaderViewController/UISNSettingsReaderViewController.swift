ac30fa2c523305da176eb3a5ee77f3781545ff40
Costin Andronache <aco@rms.ro>
2016-08-12 18:27:44 +0300

    Chrome extension functionality
    Implemented the social network settings reading functionality.
    The user will be prompt to login on each site and then javascript code
    is injected in order to extract the required settings.
    Switched to using a WKWebView

--- op-clients-ios/Operando/Operando/UISNSettingsReaderViewController/UISNSettingsReaderViewController.swift
+++ op-clients-ios/Operando/Operando/UISNSettingsReaderViewController/UISNSettingsReaderViewController.swift
@@ -11,26 +11,74 @@ import UIKit
 class UISNSettingsReaderViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var webView: UIWebView!
     
+    @IBOutlet weak var beginNewReadingButton: UIButton!
+    @IBOutlet weak var webViewHost: UIView!
     let ospSettingsManager = OSPSettingsManager()
     
+    @IBOutlet weak var snSettingsView: UISNSettingsView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginButton.alpha = 0;
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.startANewReading()
+    }
+    
+    
+    @IBAction func didPressBeginNewReading(sender: AnyObject)
+    {
+        self.startANewReading()
+    }
+    
+    func startANewReading()
+    {
+        self.beginNewReadingButton.alpha = 0.0;
+        self.loginButton.alpha = 1.0;
+        self.webViewHost.alpha = 1.0
+        self.snSettingsView.alpha = 0.0;
         
+        self.beginNewReadingWithCompletion { (results, error) in
+            
+            self.loginButton.alpha = 0.0;
+            self.beginNewReadingButton.alpha = 1.0;
+            self.webViewHost.alpha = 0.0
+            
+            if let results = results
+            {
+                self.snSettingsView.alpha = 1.0;
+                self.snSettingsView.reloadWithItems(results)
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    func beginNewReadingWithCompletion(completion: ((results: [SettingsReadResult]?, error: NSError?) -> Void)?)
+    {
         let defaultsUserAgent: [String : AnyObject] = ["UserAgent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12"]
         
         NSUserDefaults.standardUserDefaults().registerDefaults(defaultsUserAgent)
         NSUserDefaults.standardUserDefaults().synchronize()
         
-        let settingsApplier = WebViewSettingsApplier(loginFinishedButton: self.loginButton, webView: self.webView)
+        let settingsApplier = WebKitSettingsApplier(loginIsDoneButton: self.loginButton)
+        UIView.constrainView(settingsApplier.webView, inHostView: self.webViewHost)
+        self.view.bringSubviewToFront(self.loginButton)
+        
+        
         let settingsProvider = LocalJSSettingsProvider()
         
         self.ospSettingsManager.readSettingsWithProvider(settingsProvider, andApplier: settingsApplier) { (results, error) in
-            print(results)
-            print(error)
+            
+            NSUserDefaults.standardUserDefaults().removeObjectForKey("UserAgent");
+            NSUserDefaults.standardUserDefaults().synchronize();
+            
+            completion?(results: results, error: error);
         }
-        
     }
-
+    
 }
--- op-clients-ios/Operando/Operando/ios/readSNSettings.js
+++ op-clients-ios/Operando/Operando/ios/readSNSettings.js
@@ -1,36 +1,34 @@
-window.readSettings = function (settingJsonString)
+ window.readSettings =  function (settingJSONString)
 {
-    var jQeryURL = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
+    var obj = {};
     
-    if (!window.jQuery)
+    try
     {
-        var jq = document.createElement('script'); jq.type = 'text/javascript';
-        // Path to jquery.js file, eg. Google hosted version
-        jq.src = jQeryURL;
-        
-        document.getElementsByTagName('head')[0].appendChild(jq);
-    }
-    
-    var jquery_selector = JSON.parse(settingJsonString);
-    
-    if (typeof jQuery == 'undefined') {
-        return "jQuery is not loaded!";
-    } else {
-        return "jQuery is loaded!";
-    }
-    
-    return JSON.stringify(jquery_selector);
-    
-    switch (jquery_selector.valueType){
-        case "attrValue": setting = jQuery(jquery_selector.element).attr(jquery_selector.attrValue); break;
-        case "checkbox": setting = jQuery(jquery_selector.element).attr("checked")?true:false; break;
-        case "inner": setting = jQuery(jquery_selector.element).text(); break;
-        case "classname": setting = jQuery(jquery_selector.element).hasClass(jquery_selector.attrValue); break;
-        case "radio" :setting = jQuery(jquery_selector.element + ":checked").attr("value"); break;
-        case "selected": setting = jQuery(jquery_selector.element).attr("value"); break;
-        case "length": setting = jQuery(jquery_selector.element).length?jQuery(jquery_selector.element).length:0; break;
-        default: setting = null;
-    }
+   if (typeof jQuery == 'undefined') {
+          return "JQUERY IS NOT LOADED OMG";
+      } 
 
-    return JSON.stringify({settingKey:setting.settingKey, setting:setting});
-}
+      
+  var setting = JSON.parse(settingJSONString);
+  var jquery_selector = setting.jquery_selector;
+  var settingName = setting.name;
+      
+  switch (jquery_selector.valueType){
+  case "attrValue": setting = jQuery(jquery_selector.element).attr(jquery_selector.attrValue); break;
+  case "checkbox": setting = jQuery(jquery_selector.element).attr("checked")?true:false; break;
+  case "inner": setting = jQuery(jquery_selector.element).text(); break;
+  case "classname": setting = jQuery(jquery_selector.element).hasClass(jquery_selector.attrValue); break;
+  case "radio" :setting = jQuery(jquery_selector.element + ":checked").attr("value"); break;
+  case "selected": setting = jQuery(jquery_selector.element).attr("value"); break;
+  case "length": setting = jQuery(jquery_selector.element).length?jQuery(jquery_selector.element).length:0; break;
+      default: setting = {"notFound" : "true"};
+  }
+  
+      obj[settingName] = setting;
+    }catch(e)
+    {
+        obj["Exception"] = e;
+    }
+    
+  return JSON.stringify(obj);
+  };
