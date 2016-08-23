1e44f0d9ece50f707f60843c9c8848d9f87d8cf0
Costin Andronache <aco@rms.ro>
2016-08-23 18:42:32 +0300

    Facebook security enforcing
    facebook_iOS.js can be injected into the web views and affect the
    privacy settings of the Facebook account in the same manner as the
    Chrome extension.

--- op-clients-ios/Operando/Operando/UISNSettingsReaderViewController/UISNSettingsReaderViewController.swift
+++ op-clients-ios/Operando/Operando/UISNSettingsReaderViewController/UISNSettingsReaderViewController.swift
@@ -11,7 +11,7 @@ import WebKit
 
 class UISNSettingsReaderViewController: UIViewController {
 
-    var webView: UIWebView!
+    var webView: WKWebView!
     
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var beginNewReadingButton: UIButton!
@@ -27,7 +27,7 @@ class UISNSettingsReaderViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loginButton.alpha = 0;
-        self.webView = UIWebView()
+        self.webView = WKWebView()
         UIView.constrainView(self.webView, inHostView: self.webViewHost)
         self.view.bringSubviewToFront(self.loginButton)
 
