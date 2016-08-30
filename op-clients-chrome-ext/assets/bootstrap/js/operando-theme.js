96ec81cb86c9ed7c4cd4aa9998c78132d0a9e372
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-08-30 17:15:59 +0300

    added custom scrollbar + started implementing new nice theme

--- op-clients-chrome-ext/assets/bootstrap/js/operando-theme.js
+++ op-clients-chrome-ext/assets/bootstrap/js/operando-theme.js
@@ -1,41 +1,14 @@
 $(document).ready(function(){
 
-    var navbars = $(".navbar-nav");
-    var menusTabLinks = $("a[target-tab]");
-    var contentTabs = $("div.content_tab");
-
-
-    function changeHash() {
-        if(window.location.hash) {
-            if($(window.location.hash).length) {
-
-                navbars.find("li[class=active]").removeClass("active");
-                var anchor = $("a[target-tab='"+window.location.hash.replace('#','')+"']");
-                anchor.parent().addClass("active");
-                contentTabs.hide();
-                $(window.location.hash).show();
-            }
-
-        }
-    }
-
-    $.each(menusTabLinks,function(index,item){
-        $(item).on("click", function(){
-            navbars.find("li[class=active]").removeClass("active");
-            $(this).parent().addClass("active");
-            var currentTabId = $(this).attr("target-tab");
-            contentTabs.hide();
-            $('#'+currentTabId).show();
-            window.location.hash = currentTabId;
-        })
-    });
-
-
-    $(window).on('hashchange', function() {
-        changeHash();
-    });
-    changeHash();
-
+    (function($){
+        $(window).on("load",function(){
+            $("#wrapper").mCustomScrollbar({
+                scrollButtons:{enable:true},
+                theme:"light-thick",
+                scrollbarPosition:"outside"
+            });
+        });
+    })(jQuery);
 
 
 
