5267381a976109a0cbc841a46036020730341514
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2016-09-06 13:09:08 +0300

    added scroll top

--- op-clients-chrome-ext/assets/bootstrap/js/operando-theme.js
+++ op-clients-chrome-ext/assets/bootstrap/js/operando-theme.js
@@ -1,18 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    (function($){
-        $(window).on("load",function(){
+    /**
+     *Custom scrollbar
+     */
+    (function ($) {
+        $(window).on("load", function () {
             $("#wrapper").mCustomScrollbar({
-                scrollButtons:{enable:true},
-                theme:"light-thick",
-                scrollbarPosition:"outside"
+                scrollButtons: {enable: true},
+                theme: "light-thick",
+                scrollbarPosition: "outside",
+                callbacks: {
+                    whileScrolling: function () {
+                        if (this.mcs.top < -100) {
+                            $('#return-to-top').fadeIn();
+                        }
+                        else {
+                            $('#return-to-top').fadeOut();
+                        }
+                    }
+                }
+
             });
         });
     })(jQuery);
 
+    /**
+     *Scroll Up Button
+     */
+    (function ($) {
+        $('#return-to-top').click(function () {
+            $("#wrapper").mCustomScrollbar("scrollTo", "top", {
+                scrollEasing: "easeOut"
+            });
 
-
-
+        });
+    })(jQuery);
 
 })
 
