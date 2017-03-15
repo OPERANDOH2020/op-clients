86ff65ddac073cc5296867abd14893ebc6cd1847
Catalin Pomirleanu <catalin.pomarleanu@gmail.com>
2017-03-15 14:41:52 +0200

    PrivacyWizard: Added Info Screen

---
+++ op-clients-ios/PrivacyWizard/FBSecurityQuestionnaire/ExternalFrameworks/ScrollableOverlay/DisclaimerView.swift
@@ -0,0 +1,175 @@
+//
+//  DisclaimerView.swift
+//  ScrollableDisclaimerViewAlert
+//
+//  Created by Kuba on 09/03/2017.
+//  Copyright © 2017 HuD. All rights reserved.
+//
+
+import UIKit
+
+protocol DisclaimerViewDelegate: class {
+    func acceptDisclaimer()
+}
+
+class DisclaimerView: UIView {
+    
+    @IBOutlet weak var acceptButton: UIButton!
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var content: UITextView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    let kAnimationDuration = 0.3
+    
+    // MARK: Delegate
+    
+    weak var delegate: DisclaimerViewDelegate?
+    
+    @IBAction func acceptAction(_ sender: Any) {
+        delegate?.acceptDisclaimer()
+    }
+    
+    // MARK: Implementation
+    
+    class func initView() -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        return disclaimerView
+    }
+    
+    class func initWith(title: String, content: String, acceptTitle: String = "Dismiss", frame: CGRect = UIScreen.main.bounds, delegate: DisclaimerViewDelegate) -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        
+        disclaimerView.title.text = title
+        disclaimerView.content.text = content
+        disclaimerView.contentView.backgroundColor = .operandoMidBlue
+        disclaimerView.acceptButton.setTitle(acceptTitle, for: .normal)
+        disclaimerView.acceptButton.backgroundColor = .operandoBlue
+        disclaimerView.acceptButton.setTitleColor(.white, for: .normal)
+        disclaimerView.delegate = delegate
+        disclaimerView.content.isSelectable = false
+        
+        disclaimerView.frame = frame
+        
+        addGradient(toView: disclaimerView.contentView)
+        disclaimerView.backgroundColor = .lightGray
+        
+        disclaimerView.contentView.bringSubview(toFront: disclaimerView.title)
+        disclaimerView.contentView.bringSubview(toFront: disclaimerView.content)
+        disclaimerView.contentView.bringSubview(toFront: disclaimerView.acceptButton)
+        disclaimerView.contentView.bringSubview(toFront: disclaimerView.logoImageView)
+        
+        return disclaimerView
+    }
+    
+    class func initWith(title: String, filePath: String, fileExtension: String, acceptTitle: String = "Dismiss", frame: CGRect = UIScreen.main.bounds, delegate: DisclaimerViewDelegate) -> DisclaimerView {
+        
+        let disclaimerView = UINib(nibName: "Disclaimer", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! DisclaimerView
+        
+        disclaimerView.title.text = title
+
+        if let path = Bundle.main.path(forResource: filePath, ofType: fileExtension) {
+            disclaimerView.content.text = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        }
+        
+        disclaimerView.acceptButton.setTitle(acceptTitle, for: .normal)
+        
+        disclaimerView.delegate = delegate
+        
+        disclaimerView.frame = frame
+        
+        disclaimerView.acceptButton.borders(for: [.top], color: .init(colorLiteralRed: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        
+        disclaimerView.backgroundColor = .lightGray
+        
+        return disclaimerView
+    }
+    
+    override func layoutSubviews() {
+        layer.opacity = 0
+        UIView.animate(withDuration: kAnimationDuration) {
+            self.layer.opacity = 1
+        }
+    }
+    
+    override func removeFromSuperview() {
+        UIView.animate(withDuration: kAnimationDuration, animations: {
+            self.layer.opacity = 0
+        }) { processing in
+            if !processing { super.removeFromSuperview() }
+        }
+    }
+}
+
+// MARK: - Extensions
+
+extension UIView {
+    class func addGradient(toView view: UIView) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        
+        let color1 = UIColor.white.cgColor as CGColor
+        let color2 = UIColor.white.cgColor as CGColor
+        let color3 = UIColor.operandoLightBlue.cgColor as CGColor
+        let color4 = UIColor.operandoLightBlue.cgColor as CGColor
+        let color5 = UIColor.operandoMidBlue.cgColor as CGColor
+        let color6 = UIColor.operandoMidBlue.cgColor as CGColor
+        let color7 = UIColor.operandoBlue.cgColor as CGColor
+        let color8 = UIColor.operandoBlue.cgColor as CGColor
+        
+        gradientLayer.colors = [color1, color2, color3, color4, color5, color6, color7, color8]
+        gradientLayer.locations = [0.0, 0.25, 0.75, 1.0]
+        
+        view.layer.addSublayer(gradientLayer)
+    }
+    
+    func borders(for edges:[UIRectEdge], width:CGFloat = 1, color: UIColor = .black) {
+        
+        if edges.contains(.all) {
+            layer.borderWidth = width
+            layer.borderColor = color.cgColor
+        } else {
+            let allSpecificBorders:[UIRectEdge] = [.top, .bottom, .left, .right]
+            
+            for edge in allSpecificBorders {
+                if let v = viewWithTag(Int(edge.rawValue)) {
+                    v.removeFromSuperview()
+                }
+                
+                if edges.contains(edge) {
+                    let v = UIView()
+                    v.tag = Int(edge.rawValue)
+                    v.backgroundColor = color
+                    v.translatesAutoresizingMaskIntoConstraints = false
+                    addSubview(v)
+                    
+                    var horizontalVisualFormat = "H:"
+                    var verticalVisualFormat = "V:"
+                    
+                    switch edge {
+                    case UIRectEdge.bottom:
+                        horizontalVisualFormat += "|-(0)-[v]-(0)-|"
+                        verticalVisualFormat += "[v(\(width))]-(0)-|"
+                    case UIRectEdge.top:
+                        horizontalVisualFormat += "|-(0)-[v]-(0)-|"
+                        verticalVisualFormat += "|-(0)-[v(\(width))]"
+                    case UIRectEdge.left:
+                        horizontalVisualFormat += "|-(0)-[v(\(width))]"
+                        verticalVisualFormat += "|-(0)-[v]-(0)-|"
+                    case UIRectEdge.right:
+                        horizontalVisualFormat += "[v(\(width))]-(0)-|"
+                        verticalVisualFormat += "|-(0)-[v]-(0)-|"
+                    default:
+                        break
+                    }
+                    
+                    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: horizontalVisualFormat, options: .directionLeadingToTrailing, metrics: nil, views: ["v": v]))
+                    self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: verticalVisualFormat, options: .directionLeadingToTrailing, metrics: nil, views: ["v": v]))
+                }
+            }
+        }
+    }
+}
--- op-clients-ios/PrivacyWizard/FBSecurityQuestionnaire/UserInterface/UIFlowControllers/UIMainScreenFlowController.swift
+++ op-clients-ios/PrivacyWizard/FBSecurityQuestionnaire/UserInterface/UIFlowControllers/UIMainScreenFlowController.swift
@@ -8,29 +8,57 @@
 
 import UIKit
 
-protocol UIMainScreenVCDelegate {
-    func openQuestionnaire()
-}
-
-class UIMainScreenFlowController: UIFlowController, UIMainScreenVCDelegate {
+class UIMainScreenFlowController: UIFlowController, UIMainScreenVCDelegate, DisclaimerViewDelegate {
     
     let configuration : UIFlowConfiguration
     var childFlow : UIFlowController?
+    private var childViewController: UIMainViewController?
+    private lazy var infoView = DisclaimerView.initView()
     
     required init(configuration : UIFlowConfiguration) {
         self.configuration = configuration
     }
     
     func start() {
-        let mainScreenVC = UINavigationManager.getMainScreenViewController()
-        mainScreenVC.setup(delegate: self)
-        configuration.navigationController?.pushViewController(mainScreenVC, animated: true)
+        childViewController = UINavigationManager.getMainScreenViewController()
+        childViewController!.setup(delegate: self)
+        configuration.navigationController?.pushViewController(childViewController!, animated: true)
     }
     
     // Mark: - Main Screen VC Delegate Methods
     func openQuestionnaire() {
+        presentOptions()
+    }
+    
+    func displayInfo() {
+        infoView = DisclaimerView.initWith(title: "Lorem ipsum title", content: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.", delegate: self)
+        childViewController?.view.addSubview(infoView)
+    }
+    
+    private func presentOptions() {
+        guard let childViewController = childViewController else { return }
+        UIAlertViewController.presentOptionsAlert(from: childViewController, title: "Configuration options", message: "Select the social media network for setting your privacy.", actions: [
+            (title: "Facebook", callback: { (action) in
+                self.openQuestionnnaire(withScope: .facebook)
+            }),
+            (title: "LinkedIn", callback: { (action) in
+                self.openQuestionnnaire(withScope: .linkedIn)
+            }),
+            (title: "All Social Networks", callback: { (action) in
+                self.openQuestionnnaire(withScope: .all)
+            }),
+            (title: "Cancel", callback: nil)
+            ])
+    }
+    
+    private func openQuestionnnaire(withScope scope: ACPrivacyWizardScope) {
         let questionnaireTVCConfiguration = UIFlowConfiguration(window: nil, navigationController: configuration.navigationController, parent: self)
         childFlow = UIQuestionnaireFlowController(configuration: questionnaireTVCConfiguration)
+        (childFlow as? UIQuestionnaireFlowController)?.setup(withPrivacyWizardScope: scope)
         childFlow?.start()
+    }
+    
+    func acceptDisclaimer() {
+        infoView.removeFromSuperview()
     }
 }
